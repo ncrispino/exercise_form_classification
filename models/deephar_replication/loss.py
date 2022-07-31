20f0a1dfa0cd4dec3919e2004087f84bb6ddad8a
ncrispino <ncrispino@wustl.edu>
2022-07-31 13:34:07 -0500

    trying to change loss so weights update

--- models/deephar_replication/loss.py
+++ models/deephar_replication/loss.py
@@ -19,7 +19,17 @@ class ElasticNetLoss():
         self.include_bincross = include_bincross
 
     def __call__(self, pose_pred, pose_true):
-        N_J = pose_pred.shape[-1]        
+        # Non-visible and outer joints shouldn't be used in the loss. 
+        # In preprocessing, they are set as -1e9 if they are either.        
+        # idx = torch.nonzero(pose_true[:, :-1, :, :] > 0)
+        idx = pose_true > 0
+        # poses = pose_pred[idx]
+        poses = torch.where(idx, pose_pred, 0 * pose_pred)
+        print(poses.shape)
+        print(poses)        
+        N_J = idx.sum(-1)[:, :, 0]
+        print('Idx: ' + str(idx))       
+        print('N_J: ' + str(N_J))
         # Calculate norm across joint coordinates and avg over them: 
         # B x 3 x T x N_J -> B x T x N_J -> B x T.
         if self.include_bincross:
@@ -28,6 +38,14 @@ class ElasticNetLoss():
             pose_diff = pose_pred[:, :-1] - pose_true[:, :-1]
         else:
             bc = 0
-            pose_diff = pose_pred - pose_true        
-        loss = (torch.norm(pose_diff, dim=(1), p=1) + torch.norm(pose_diff, dim=(1), p=2)**2 + bc).mean()     
+            pose_diff = pose_pred - pose_true
+
+        # print(pose_pred)
+        # print(pose_true)
+        # # print(pose_diff) 
+        # print(pose_diff.abs().sum(axis=1))
+        # print((pose_diff**2).sum(axis=1))
+        loss_by_frame = torch.norm(pose_diff, dim=(1), p=1) + torch.norm(pose_diff, dim=(1), p=2)**2 + bc
+        # Zero out loss for non-visible and outer joints if applicable.
+        loss = (torch.where(idx, loss_by_frame, 0 * pose_pred)).sum()/N_J
         return loss
